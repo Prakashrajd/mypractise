e46937792103c90dbd4f35f7744426af59598596
prakash <prakashrajd40@gmail.com>
2021-09-28 11:42:39 +0530

    sum

--- src/com/myLearnings/basics/java/recursion2.java
+++ src/com/myLearnings/basics/java/recursion2.java
@@ -1,5 +1,28 @@
 package com.myLearnings.basics.java;
 
 public class recursion2 {
+ int a=1;
 
+void m1()
+{
+	System.out.print("m1");
+	if(a<5){
+		a++;
+		m1();
+	}
+}
+void m2()
+{
+  m3();
+  System.out.print("m2");
+}
+ void m3()
+ {
+	 System.out.print("m3");
+ }
+ public static void main(String args[]) {
+	 recursion2 b=new recursion2();
+	 b.m1();
+ 
+ }
 }
